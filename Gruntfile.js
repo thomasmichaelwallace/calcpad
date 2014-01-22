1b4e55e29beb1c8d31ccad14d822471c4ef20272
thomasmichaelwallace <thomasmichaelwallace@googlemail.com>
2014-01-22 22:17:48 +0000

    Added Grunt, RequireJS and Underscore deps.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        
+        pkg: grunt.file.readJSON('package.json'),
+        
+        // bower-task
+        bower: {
+            install: {
+                options: {
+                    targetDir: './public/lib',
+                    layout: 'byComponent',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        },
+
+        // contrib-connect server
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: 'public',
+                    keepalive: true
+                }
+            }
+        }
+
+    });
+
+    // Keep bower and public seperate.
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    // Provide a development server.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect']);
+
+};
