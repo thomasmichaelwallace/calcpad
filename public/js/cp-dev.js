4c70c8ec29c8971f9747fed06c408a6fbc7b6d07
thomasmichaelwallace <thomasmichaelwallace@googlemail.com>
2014-01-26 22:25:16 +0000

    Now uses Backbone.js for MVC.

--- public/js/cp-dev.js
+++ public/js/cp-dev.js
@@ -1,110 +1,232 @@
-// Development Scratch for CalcPad
+// Development script for CalcPad
 
 // This file will ultimately grow into modules, and all that jazz.
 // But for now it's just a simple one-page testament to bad programming.
 
-$.getJSON("/json/beam.json", function( data ) {
-	// Start by fetching predetermined data on load; this will change with time.
-	renderPad(data);
-});
+$(function() {
 
-function renderPad( pad ) {
-	// Renders each line of the pad, using that object type's renderer.
+	// Abstract line model.
+	var LineItem = Backbone.Model.extend({
 
-	console.log("[CalcPad] Rendering: " + pad.name)	;
-
-	for (var lineNo = 0; lineNo < pad.lines.length; lineNo++) {
-		var line = pad.lines[lineNo];
-
-		var $selectors = $('<a class="list-group-item"></a>').appendTo($('#Pad'));		
-		lineTypers[line.lineType].render( line, $selectors );
-	}
-}
-
-var lineTypers = {
-	// Modules for each line-types.
-
-	Title: {
-		templates: {
-			view: _.template('<h3><%= text %></h3>')
-		},
-		render: function( line, parent ) {
-			parent.append(this.templates.view(line));
+		defaults: function() {
+			return {
+				lineType: "Blank"
+			};
 		}
-	},
-	SectionTitle: {
-		templates: {
-			view: _.template('<h4><%= text %></h4>')
+
+	});
+
+	// Abstract line view.
+	var LineView = Backbone.View.extend({
+
+		tagName: "a",
+		className: "list-group-item",
+
+		template: _.template('<p><%= lineType %></p>'),
+
+		initialize: function() {
+			this.listenTo(this.model, 'change', this.render);
 		},
-		render: function( line, parent ) {
-			parent.append(this.templates.view(line));
-		}
-	},	
-	Paragraph: {
-		templates: {
-			view: _.template('<p><%= formattedText %></p>')
+
+		prerender: function() { },
+		postrender: function() { },
+
+		render: function() {
+			this.prerender();
+			this.$el.html(this.template(this.model.toJSON()));
+			this.postrender();
+			return this;
 		},
-		render: function( line, parent ) {
-			line.formattedText = line.bodyText.replace('\n', '<p></p>');
-			parent.append(this.templates.view(line));			
-		}
-	},
-	Image: {
-		templates: {
-			view: _.template('<img src="<%= image %>" class="img-rounded img-responsive img-center">')
+
+	});
+
+	// Implementations of line types.
+	var lineTypes = {
+
+		Title: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "Title",
+						text: "New Title"
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template('<h3><%= text %></h3>')
+			})
 		},
-		render: function( line, parent ) {
-			parent.append(this.templates.view(line));
-		}		
-	},
-	Input: {
-		templates: {
-			view: _.template([
-				'<div class="form-horizontal">',
-					'<div class="form-group list-form-group">',
-						'<div class="col-sm-7">',
-							'<p class="form-control-static"><%= description %></p>',
-						'</div>',
-						'<div class="col-sm-5">',
-							'<div class="input-group">',
-								'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
-								'<input type="text" class="form-control" placeholder="<%= value %>">',
-								'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= unit %>}\\)</span>',								
+
+		SectionTitle: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "SectionTitle",
+						text: "New Title"
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template('<h4><%= text %></h4>')
+			})
+		},
+
+		Paragraph: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "Paragraph",
+						bodyText: "New Paragraph"
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template('<p><%= formattedText %></p>'),
+				prerender: function() {
+					this.model.set('formattedText', this.model.get('bodyText').replace('\n', '<p></p>'));
+				}
+			})
+		},
+
+		Image: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "Image"
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template('<img src="<%= image %>" class="img-rounded img-responsive img-center">')
+			})
+		},
+
+		Input: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "Input",
+						description: "New Parameter",
+						symbol: "P",
+						value: 0,
+						unit: "kN",
+						scope: ""
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template([
+					'<div class="form-horizontal">',
+						'<div class="form-group list-form-group">',
+							'<div class="col-sm-7">',
+								'<p class="form-control-static"><%= description %></p>',
+							'</div>',
+							'<div class="col-sm-5">',
+								'<div class="input-group">',
+									'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
+									'<input type="text" class="form-control" placeholder="<%= value %>">',
+									'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= unit %>}\\)</span>',
+								'</div>',
 							'</div>',
 						'</div>',
+					'</div>'
+				].join('\n'))
+			}),
+			postrender: function() {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub, this]);
+			}
+		},
+
+		Formula: {
+			model: LineItem.extend({
+				defaults: function() {
+					return {
+						lineType: "Formula",
+						description: "New Parameter",
+						symbol: "P",
+						latex: "0",
+						formula: "0.0",
+						unit: "kN",
+						scope: "",
+						value: 0
+					};
+				}
+			}),
+			view: LineView.extend({
+				template: _.template([
+					'<p><%= description %></p>',
+					'<div class="latex-line">',
+						'\\[<%= symbol %> = <%= latex %>\\]',
 					'</div>',
-				'</div>'
-			].join('\n'))
-		},		
-		render: function( line, parent ) {
-			parent.append(this.templates.view(line));			
-			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-		}
-	},
-	Formula: {
-		templates: {
-			view: _.template([
-				'<p><%= description %></p>',
-				'<div class="latex-line">',
-					'\\[<%= symbol %> = <%= latex %>\\]',
-				'</div>',
-				'<div class="form-horizontal">',
-					'<div class="form-group list-form-group">',
-						'<div class="col-sm-7">',
+					'<div class="form-horizontal">',
+						'<div class="form-group list-form-group">',
+							'<div class="col-sm-7">',
+							'</div>',
+							'<div class="col-sm-5">',
+								'<div class="input-group">' ,
+									'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
+								'<p class="form-control"><%= value %></p>',
+								'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= units %>}\\)</span>',
+							'</div>',
 						'</div>',
-						'<div class="col-sm-5">',
-							'<div class="input-group">' ,
-								'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
-							'<p class="form-control"><%= value %></p>',
-							'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= units %>}\\)</span>',
-						'</div>',
-					'</div>',							
-				'</div>'
-			].join('\n'))
-		},		
-		render: function( line, parent ) {
-			parent.append(this.templates.view(line));			
-			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+					'</div>'
+				].join('\n'))
+			}),
+			postrender: function() {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub, this]);
+			}
 		}
-	}
-};
+	};
+
+
+	// Collection of lines forming a single pad.
+	var LineCollection = Backbone.Collection.extend({
+
+		model: function (attrs, options) {
+			var type = attrs.lineType;
+			if (type in lineTypes) {
+				return new lineTypes[type].model(attrs, options);
+			} else {
+				return new LineItem(attrs, options);
+			}
+		},
+
+		url: 'json/beam.json',
+
+	});
+	var Lines = new LineCollection();
+
+
+	// CalcPad Application view
+	var PadView = Backbone.View.extend({
+
+		el: $(".container"),
+
+		initialize: function() {
+			this.listenTo(Lines, 'add', this.addOne);
+			this.listenTo(Lines, 'reset', this.addAll);
+			this.listenTo(Lines, 'all', this.render);
+
+			Lines.fetch();
+		},
+
+		addOne: function(line) {
+			var type = line.get('lineType');
+			var view;
+			if (type in lineTypes) {
+				view = new lineTypes[type].view({ model: line });
+			} else {
+				view = new LineView({ model: line });
+			}
+			this.$("#pad").append(view.render().el);
+		},
+
+		// Add all items in the **Todos** collection at once.
+		addAll: function() {
+			Lines.each(this.addOne, this);
+		},
+	});
+
+	var Pad = new PadView();
+
+});
