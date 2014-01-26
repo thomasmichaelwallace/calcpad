d47c0e5ef3cad38998913b2e7fb1b6e5c64c7867
thomasmichaelwallace <thomasmichaelwallace@googlemail.com>
2014-01-26 16:19:02 +0000

    Use templates to render pad.

--- public/js/cp-dev.js
+++ public/js/cp-dev.js
@@ -6,7 +6,7 @@
 $.getJSON("/json/beam.json", function( data ) {
 	// Start by fetching predetermined data on load; this will change with time.
 	renderPad(data);
-})
+});
 
 function renderPad( pad ) {
 	// Renders each line of the pad, using that object type's renderer.
@@ -16,7 +16,7 @@ function renderPad( pad ) {
 	for (var lineNo = 0; lineNo < pad.lines.length; lineNo++) {
 		var line = pad.lines[lineNo];
 
-		var $selectors = $('<a href="#" class="list-group-item"></a>').appendTo($('#Pad'));		
+		var $selectors = $('<a class="list-group-item"></a>').appendTo($('#Pad'));		
 		lineTypers[line.lineType].render( line, $selectors );
 	}
 }
@@ -25,68 +25,86 @@ var lineTypers = {
 	// Modules for each line-types.
 
 	Title: {
+		templates: {
+			view: _.template('<h3><%= text %></h3>')
+		},
 		render: function( line, parent ) {
-			parent.append('<h3>' + line.text + '</h3>');
+			parent.append(this.templates.view(line));
 		}
 	},
 	SectionTitle: {
+		templates: {
+			view: _.template('<h4><%= text %></h4>')
+		},
 		render: function( line, parent ) {
-			parent.append('<h4>' + line.text + '</h4>');
+			parent.append(this.templates.view(line));
 		}
 	},	
 	Paragraph: {
+		templates: {
+			view: _.template('<p><%= formattedText %></p>')
+		},
 		render: function( line, parent ) {
-			parent.append('<p>' + line.bodyText.replace('\n', '<p></p>') + '</p>');
-
+			line.formattedText = line.bodyText.replace('\n', '<p></p>');
+			parent.append(this.templates.view(line));			
 		}
 	},
 	Image: {
+		templates: {
+			view: _.template('<img src="<%= image %>" class="img-rounded img-responsive img-center">')
+		},
 		render: function( line, parent ) {
-			parent.append('<img src="' + line.image + '" class="img-rounded img-responsive img-center">');
-		}
+			parent.append(this.templates.view(line));
+		}		
 	},
 	Input: {
-		render: function( line, parent ) {
-			parent.append(
-				'<div class="form-horizontal">' + 
-					'<div class="form-group list-form-group">' +
-						'<div class="col-sm-7">' +
-							'<p class="form-control-static">' + line.description + '</p>' + 
-						'</div>' +
-						'<div class="col-sm-5">' +
-							'<div class="input-group">' +
-								'<span class="input-group-addon latex-addon">\\(' + line.symbol + '\\)</span>' +
-								'<input type="text" class="form-control" placeholder="' + line.default + '">' +
-								'<span class="input-group-addon latex-addon">\\(\\mathrm{' + line.unit + '}\\)</span>' +
-							'</div>' +
-						'</div>' +
-					'</div>' +
+		templates: {
+			view: _.template([
+				'<div class="form-horizontal">',
+					'<div class="form-group list-form-group">',
+						'<div class="col-sm-7">',
+							'<p class="form-control-static"><%= description %></p>',
+						'</div>',
+						'<div class="col-sm-5">',
+							'<div class="input-group">',
+								'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
+								'<input type="text" class="form-control" placeholder="<%= value %>">',
+								'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= unit %>}\\)</span>',								
+							'</div>',
+						'</div>',
+					'</div>',
 				'</div>'
-			);
+			].join('\n'))
+		},		
+		render: function( line, parent ) {
+			parent.append(this.templates.view(line));			
 			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 		}
 	},
 	Formula: {
-		render: function( line, parent ) {
-			var value = 62.5;
-			parent.append(
-				'<p>' + line.description + '</p>' +
-				'<div class="latex-line">' +
-					'\\[' + line.symbol + '=' + line.latex + '\\]' +
-				'</div>' +
-				'<div class="form-horizontal">' +
-					'<div class="form-group list-form-group">' +
-						'<div class="col-sm-7">' +
-						'</div>' +
-						'<div class="col-sm-5">' +
-							'<div class="input-group">' + 
-							'<span class="input-group-addon latex-addon">\\(' + line.symbol + '\\)</sub></span>' +
-							'<p class="form-control">' + value + '</p>' +
-							'<span class="input-group-addon latex-addon">\\(\\mathrm{' + line.units + '}\\)</span>' +
-						'</div>' +
-					'</div>' +
+		templates: {
+			view: _.template([
+				'<p><%= description %></p>',
+				'<div class="latex-line">',
+					'\\[<%= symbol %> = <%= latex %>\\]',
+				'</div>',
+				'<div class="form-horizontal">',
+					'<div class="form-group list-form-group">',
+						'<div class="col-sm-7">',
+						'</div>',
+						'<div class="col-sm-5">',
+							'<div class="input-group">' ,
+								'<span class="input-group-addon latex-addon">\\(<%= symbol %>\\)</span>',
+							'<p class="form-control"><%= value %></p>',
+							'<span class="input-group-addon latex-addon">\\(\\mathrm{<%= units %>}\\)</span>',
+						'</div>',
+					'</div>',							
 				'</div>'
-			);
+			].join('\n'))
+		},		
+		render: function( line, parent ) {
+			parent.append(this.templates.view(line));			
+			MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 		}
 	}
-}
+};
